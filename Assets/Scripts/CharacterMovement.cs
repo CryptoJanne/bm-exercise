870e73193d5272d07812703f4a7dda57c49e45e3
Max <maxlundh91@protonmail.com>
2021-08-29 21:25:32 +0200

    Background and IdleAnimation

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private void Update()
+    {
+        float speed = 40f;
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveY = +1f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveY = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX = +1f;
+        }
+
+        Vector3 direction = new Vector3(moveX, moveY);
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
