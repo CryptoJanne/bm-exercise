4a2242d1cc3969aaae736e83757b437f9beb4aba
Max <maxlundh91@protonmail.com>
2021-08-30 10:45:13 +0200

    Started animating

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    private Animator _anim;
+    private Rigidbody2D _rb2d;
+    private Vector2 lastMoveDir;
+    [SerializeField] private int speed;
+
+    private void Awake()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+    }
+
     private void Update()
     {
-        float speed = 40f;
         float moveX = 0f;
         float moveY = 0f;
 
@@ -31,7 +41,21 @@ public class CharacterMovement : MonoBehaviour
             moveX = +1f;
         }
 
-        Vector3 direction = new Vector3(moveX, moveY);
-        transform.position += direction * speed * Time.deltaTime;
+        Vector3 direction = new Vector3(moveX, moveY).normalized;
+        bool IsIdle = moveX == 0 && moveY == 0;
+        if (IsIdle)
+        {
+            //idle
+            _anim.SetBool("IsMoving", false);
+            _rb2d.velocity = Vector2.zero;
+        }
+        else
+        {
+            //moving
+            _anim.SetBool("IsMoving", true);
+            _anim.SetFloat("Horizontal", direction.x);
+            _rb2d.velocity = direction * speed;
+        }
+        //transform.position += direction * speed * Time.deltaTime;
     }
 }
